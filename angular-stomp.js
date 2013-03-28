fb5275af0191c5ec9532f21c19f400b9798fba2f
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2013-03-28 16:41:18 -0400

    Update to allow the url to be passed into a constructor for the service (the service returns a constructor).

--- angular-stomp.js
+++ angular-stomp.js
@@ -6,49 +6,49 @@
 angular.module('AngularStomp', []).
     factory('ngstomp', function($rootScope) {
         Stomp.WebSocketClass = SockJS;
-        var stompClient = Stomp.client('http://localhost:15674/stomp');
+        var stompClient = {};
 
-        return {
-            subscribe: function(queue, callback) {
-                stompClient.subscribe(queue, function() {
-                    var args = arguments;
-                    $rootScope.$apply(function() {
-                        callback(args[0]);
-                    })
-                })
-            },
-
-            send: function(queue, headers, data) {
-                stompClient.send(queue, headers, data);
-            },
-
-            connect: function(user, password, on_connect, on_error, vhost) {
-                stompClient.connect(user, password,
-                    function(frame) {
-                        $rootScope.$apply(function() {
-                            on_connect.apply(stompClient, frame);
-                        })
-                    },
-                    function(frame) {
-                        $rootScope.$apply(function() {
-                            on_error.apply(stompClient, frame);
-                        })
-                    }, vhost);
-            },
-
-            disconnect: function(callback) {
-                stompClient.disconnect(function() {
-                    var args = arguments;
-                    $rootScope.$apply(function() {
-                        callback.apply(args);
-                    })
-                })
-            }
+        function NGStomp(url) {
+            this.stompClient = Stomp.client(url);
         }
 
-//        function NGStomp(url, socketClass) {
-//            Stomp.WebSocketClass = socketClass;
-//            var stompClient = Stomp.client(url);
-//        }
+        NGStomp.prototype.subscribe = function(queue, callback) {
+            this.stompClient.subscribe(queue, function() {
+                var args = arguments;
+                $rootScope.$apply(function() {
+                    callback(args[0]);
+                })
+            })
+        }
 
+        NGStomp.prototype.send = function(queue, headers, data) {
+            this.stompClient.send(queue, headers, data);
+        }
+
+        NGStomp.prototype.connect = function(user, password, on_connect, on_error, vhost) {
+            this.stompClient.connect(user, password,
+                function(frame) {
+                    $rootScope.$apply(function() {
+                        on_connect.apply(stompClient, frame);
+                    })
+                },
+                function(frame) {
+                    $rootScope.$apply(function() {
+                        on_error.apply(stompClient, frame);
+                    })
+                }, vhost);
+        }
+
+        NGStomp.prototype.disconnect = function(callback) {
+            this.stompClient.disconnect(function() {
+                var args = arguments;
+                $rootScope.$apply(function() {
+                    callback.apply(args);
+                })
+            })
+        }
+
+        return function(url) {
+            return new NGStomp(url);
+        }
     });
