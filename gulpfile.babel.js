da71e13100d0a910dd8e4c43a0468eb3a3236ff6
Davin Kevin <davin.kevin@gmail.com>
2015-12-21 05:44:43 +0100

    chore(build): add jspm parameters

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -39,7 +39,7 @@ gulp.task('watch', function() {
 
 
 gulp.task('coveralls', () => {
-    gulp.src('coverage/lcov.info')
+    gulp.src('reports/coverage/**/lcov.info')
         .pipe(coveralls());
 });
 
