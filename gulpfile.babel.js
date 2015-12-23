ad81f57847d03c229beb151f3a2602557578be95
Davin Kevin <davin.kevin@gmail.com>
2015-12-23 14:22:03 +0100

    chore(build): reduce gulp task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,34 +1,12 @@
 // Include gulp and Our Plugins
 import gulp from 'gulp';
-import rename from 'gulp-rename';
-import uglify from 'gulp-uglify';
-import plumber from 'gulp-plumber';
-import ngAnnotate from 'gulp-ng-annotate';
-import babel from 'gulp-babel';
-import sourcemaps from 'gulp-sourcemaps';
-import wrap from 'gulp-wrap-js';
-import packagejson from './package.json';
 import coveralls from 'gulp-coveralls';
 
 let jsLocation = 'lib/*.js',
     jsDestination = 'dist/';
 
-let version = packagejson.version;
-
 gulp.task('scripts', function() {
-    gulp.src(jsLocation)
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(babel())
-        .pipe(wrap(`/*! AngularStompDK v${version} */
-                    (function() {%= body %})()`))
-        .pipe(ngAnnotate())
-        .pipe(rename('angular-stomp.js'))
-        .pipe(gulp.dest(jsDestination))
-        .pipe(uglify({ preserveComments : 'some' }))
-        .pipe(rename('angular-stomp.min.js'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(jsDestination));
+    console.log('scripts');
 });
 
 // Watch Files For Changes
